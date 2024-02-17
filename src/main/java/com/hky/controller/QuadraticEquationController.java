95e426777e07f9750cc20555283578947997b6a1
HariKrishnaYadav <Harikrishna.bhuit15@gmail.com>
2024-02-17 18:23:17 +0530

    second commit

--- src/main/java/com/hky/controller/QuadraticEquationController.java
+++ src/main/java/com/hky/controller/QuadraticEquationController.java
@@ -1,5 +1,6 @@
 package com.hky.controller;
 
+import com.hky.service.QuadraticEquationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,21 +8,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hky.service.QuadraticEquationService;
-
 // QuadraticEquationController.java
 @RestController
 @RequestMapping("/quadratic")
 public class QuadraticEquationController {
 
-	@Autowired
-	private QuadraticEquationService quadraticEquationService;
+    @Autowired
+    private QuadraticEquationService quadraticEquationService;
 
-	@GetMapping("/roots")
-	public ResponseEntity<double[]> calculateRoots(@RequestParam double a, @RequestParam double b,
-			@RequestParam double c) {
+    @GetMapping("/roots")
+    public ResponseEntity<double[]> calculateRoots(@RequestParam double a, @RequestParam double b,
+                                                   @RequestParam double c) {
 
-		double[] roots = quadraticEquationService.calculateRoots(a, b, c);
-		return ResponseEntity.ok(roots);
-	}
+        double[] roots = quadraticEquationService.calculateRoots(a, b, c);
+        return ResponseEntity.ok(roots);
+    }
+
+    public String getApp() {
+        return null;
+    }
+
 }
