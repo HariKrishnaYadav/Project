d7d1db64fac1400cf40d646b2725f7ef195659be
HariKrishnaYadav <Harikrishna.bhuit15@gmail.com>
2024-02-17 18:00:19 +0530

    second commit

--- src/main/java/com/hky/controller/QuadraticEquationController.java
+++ src/main/java/com/hky/controller/QuadraticEquationController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.hky.service.QuadraticEquationService;
 
-// QuadraticEquationController.java
+
 @RestController
 @RequestMapping("/quadratic")
 public class QuadraticEquationController {
